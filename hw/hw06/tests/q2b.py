f641d6ab9bd1743ad76576654e339f1a8fd49b9a
ZlatanCN <wy1604010213@163.com>
2024-04-17 13:41:45 +0800

    finished

---
+++ hw/hw06/tests/q2b.py
@@ -0,0 +1,8 @@
+OK_FORMAT = True
+test = {   'name': 'q2b',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> mask_data.shape == (3141, 6) or mask_data.shape == (3141, 7)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw06/tests/q6b.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+test = {   'name': 'q6b',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> train_rmse_improved_cpc <= 0.005\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> test_rmse_improved_cpc <= 0.005\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
